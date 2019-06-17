1609bf5e244411b29aaa7e65be61687d5cac396e
v13nr2 <nanang.programmer@gmail.com>
2019-06-18 01:01:55 +0800

    kalender piutang

--- modules/calendar/config.php
+++ modules/calendar/config.php
@@ -1,7 +1,7 @@
 <?php
 define('SQL_HOST',     'localhost');
-define('SQL_USER',     'sima');
-define('SQL_PASSWD',   'sima');
+define('SQL_USER',     'root');
+define('SQL_PASSWD',   '');
 define('SQL_DATABASE', 'sima');
 define('SQL_PREFIX',   'phpc_');
 define('SQL_TYPE',     'mysqli');
