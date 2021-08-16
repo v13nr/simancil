22c6f09d047648ee9968872995cf8c0590959be5
nanangtokenku <81137154+nanangtokenku@users.noreply.github.com>
2021-08-16 08:54:50 +0800

    page ritase barang

--- config_sistem_i.php
+++ config_sistem_i.php
@@ -1,5 +1,6 @@
 <?php 
 
+//$base_url = "";
 
 include "mysqli/class/MysqliDb.php";
 $db = new MysqliDb ('localhost', 'root', '99', 'simapos2') or die("unhost");
---
+++ modules/pos/ritasebarang/ritase.php
@@ -0,0 +1,43 @@
+      <script src="../../../bootstrap5/js/jquery.min.js"></script>
+      <script src="../../../bootstrap5/js/bootstrap.js"></script>
+      <script src="../../../bootstrap5/js/jquery.validate.min.js"></script>
+      <script src="../../../bootstrap5/js/sweetalert.min.js"></script>
+      <link href="../../../bootstrap5/css/bootstrap.css" rel="stylesheet">
+      <link href="../../../bootstrap5/css/app.css" rel="stylesheet">
+	  
+	  <div class="row">
+		<div class="col col-md-4">
+			<div class="card" style="width: 18rem;">
+			  <img src="../../../assets/images/berangkat.jpg" class="card-img-top" alt="...">
+			  <div class="card-body">
+				<h5 class="card-title">Berangkat</h5>
+				<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+				<a href="#" class="btn btn-primary">Go Entri</a>
+			  </div>
+			</div> 
+		</div>
+	  
+		<div class="col col-md-4">
+			<div class="card" style="width: 18rem;">
+			  <img src="../../../assets/images/pulang.jpg" class="card-img-top" alt="...">
+			  <div class="card-body">
+				<h5 class="card-title">Laporan Pulang</h5>
+				<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+				<a href="#" class="btn btn-primary">Go Reporting</a>
+			  </div>
+			</div>
+		</div>
+	  
+		<div class="col col-md-4">
+			<div class="card" style="width: 18rem;">
+			  <img src="../../../assets/images/cetak.jpg" class="card-img-top" alt="...">
+			  <div class="card-body">
+				<h5 class="card-title">Cetak Laporan</h5>
+				<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+				<a href="#" class="btn btn-primary">Laporan</a>
+			  </div>
+			</div>
+		</div>
+	  
+	  </div>
+ 
