64eb48f290d245990e58c83333d047048dbc89d9
nanangtokenku <81137154+nanangtokenku@users.noreply.github.com>
2021-08-16 10:37:21 +0800

    Update ritase.php

--- modules/pos/ritasebarang/ritase.php
+++ modules/pos/ritasebarang/ritase.php
@@ -11,8 +11,8 @@
 			  <img src="../../../assets/images/berangkat.jpg" class="card-img-top" alt="...">
 			  <div class="card-body">
 				<h5 class="card-title">Berangkat</h5>
-				<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-				<a href="#" class="btn btn-primary">Go Entri</a>
+				<p class="card-text">Persiapan Ritase Penjualan Salesman</p>
+				<a href="#" class="btn btn-primary"  data-bs-toggle="modal" data-bs-target="#exampleModal">Go Entri</a>
 			  </div>
 			</div> 
 		</div>
@@ -22,7 +22,7 @@
 			  <img src="../../../assets/images/pulang.jpg" class="card-img-top" alt="...">
 			  <div class="card-body">
 				<h5 class="card-title">Laporan Pulang</h5>
-				<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+				<p class="card-text">Laporan hasil penjualan ritase</p>
 				<a href="#" class="btn btn-primary">Go Reporting</a>
 			  </div>
 			</div>
@@ -33,7 +33,7 @@
 			  <img src="../../../assets/images/cetak.jpg" class="card-img-top" alt="...">
 			  <div class="card-body">
 				<h5 class="card-title">Cetak Laporan</h5>
-				<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+				<p class="card-text">Manaje dan Pelaporan Salesman</p>
 				<a href="#" class="btn btn-primary">Laporan</a>
 			  </div>
 			</div>
@@ -41,3 +41,53 @@
 	  
 	  </div>
  
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+		Hari/Tgl. <input type="date"> &nbsp; &nbsp; &nbsp; Nama Sales <input type="text" size="16"><br><br>
+        <table width="100%" border="1">
+			<tr>
+				<td>No.</td>
+				<td>Volume</td>
+				<td>Jenis Barang</td>
+				<td>Harga Satuan</td>
+				<td>Jumlah</td>
+			</tr>
+			<?php
+			
+			include "../../../config_sistem_i.php";
+
+			$cols = Array ("namabrg", "kodebrg");
+			$users = $db->get ("stock", null, $cols);
+			if ($db->count > 0)
+				foreach ($users as $user) { 
+				?>
+			   
+			   
+					<tr>
+						<td><?php echo ++$no;?>. </td>
+						<td><input type="text" size="2"></td>
+						<td><?php echo $user["namabrg"];?></td>
+						<td><input type="text" size="12"></td>
+						<td><input type="text" size="12"></td>
+					</tr>
+					
+					
+			   <?php
+				}
+			?>
+		</table>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
